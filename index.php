d242a24afc2c43d81636484f180d471da35d9400
OishiShota <s92.oishi.shota@gmail.com>
2021-03-17 17:44:09 +0900

    lesson9,10

--- index.php
+++ index.php
@@ -116,6 +116,7 @@
       echo "あなたは30歳未満です。";
     }
   ?>
+  <br>
 
   <!-- lesson8_&&,|| -->
   <?php
@@ -134,6 +135,41 @@
       echo "xは7の倍数でも3の倍数でもありません。";
     }
   ?>
+  <br>
+
+  <!-- lesson9_switch文 -->
+  <?php
+    // 変数$numを定義し、好きな数字を代入してください
+    $num = 1;
+    // 変数$remainderを定義し、変数$numを3で割った時の余りを代入してください
+    $remainder = $num % 3;
+    // switch文を用いてください
+    switch($remainder){
+      case 0:
+        echo "大吉です。";
+        break;
+      case 1:
+        echo "中吉です。";
+        break;
+      case 2:
+        echo "小吉です。";
+        break;
+      default:
+        echo "凶です。";
+        break;
+    }
+  ?>
+  <br>
+
+  <!-- lesson10_配列 -->
+  <?php
+    // この下に配列を作ってください
+    $colors = ["赤","青","黄"];
+    echo $colors[0];
+    $colors[] = "白";
+    echo $colors[3];
+  ?>
+  <br>
 
 </body>
 </html>
